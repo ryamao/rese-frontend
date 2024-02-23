755a54c6b8f91862e029499caaf351f7742982d7
ryamao <yamao.ryouta@gmail.com>
2024-02-23 13:24:14 +0900

    Vitestを導入する

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vitest/config";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["src/vitest-setup.ts"],
+    include: ["src/**/*.test.{ts,tsx}"]
+  }
+});
