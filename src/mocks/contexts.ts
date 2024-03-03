fe415ef4b94f8e3ec865cd82688c872a236c7064
ryamao <yamao.ryouta@gmail.com>
2024-03-03 17:15:55 +0900

    BackendAccessContextに飲食店一覧取得機能を追加する

---
+++ src/mocks/contexts.ts
@@ -0,0 +1,28 @@
+import {
+  GetAreasResult,
+  GetAuthStatusResult,
+  GetGenresResult
+} from "../Client";
+import { BackendAccessContextType } from "../contexts/BackendAccessContext";
+
+export interface CreateMockBackendAccessStateProps {
+  authStatus?: GetAuthStatusResult;
+  getAreas?: () => Promise<GetAreasResult["areas"]>;
+  getGenres?: () => Promise<GetGenresResult["genres"]>;
+}
+
+export function createMockBackendAccessState({
+  authStatus = { status: "guest" },
+  getAreas,
+  getGenres
+}: CreateMockBackendAccessStateProps): BackendAccessContextType {
+  return {
+    authStatus,
+    logout: vitest.fn(),
+    getAreas: getAreas ?? vitest.fn(),
+    getGenres: getGenres ?? vitest.fn(),
+    getShops: vitest.fn(),
+    addFavorite: vitest.fn(),
+    removeFavorite: vitest.fn()
+  };
+}
