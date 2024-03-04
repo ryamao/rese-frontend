24106b643df199aa7a315986216d81131ea6fb5f
ryamao <yamao.ryouta@gmail.com>
2024-03-04 17:31:01 +0900

    飲食店詳細ページの左側外観を追加する

---
+++ src/components/ShopDetail.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { ShopDetail } from "./ShopDetail";
+
+const meta = {
+  title: "Components/Shop/ShopDetail",
+  component: ShopDetail,
+  tags: ["autodocs"],
+  args: {
+    id: 1,
+    name: "仙人",
+    image_url: "https://via.placeholder.com/800x500",
+    area: { id: 1, name: "東京都" },
+    genre: { id: 1, name: "寿司" },
+    detail:
+      "料理長厳選の食材から作る寿司を用いたコースをぜひお楽しみください。食材・味・価格、お客様の満足度を徹底的に追及したお店です。特別な日のお食事、ビジネス接待まで気軽に使用することができます。",
+    favorite_status: "unknown"
+  }
+} satisfies Meta<typeof ShopDetail>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
