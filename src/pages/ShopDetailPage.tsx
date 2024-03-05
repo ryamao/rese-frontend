d1cc77e81c268a7618ad3561daf32b3ef1b1625f
ryamao <yamao.ryouta@gmail.com>
2024-03-05 15:33:54 +0900

    飲食店一覧ページから渡された飲食店情報を表示する処理を飲食店詳細ページに追加する

--- src/pages/ShopDetailPage.tsx
+++ src/pages/ShopDetailPage.tsx
@@ -1,5 +1,5 @@
 import { css } from "@emotion/css";
-import { useParams } from "react-router-dom";
+import { Location, useLocation } from "react-router-dom";
 
 import { PageBase } from "./PageBase";
 import { ShopDetailArea } from "../components/ShopDetailArea";
@@ -7,26 +7,16 @@ import { ShopReservationArea } from "../components/ShopReservationArea";
 import { ShopData } from "../models";
 
 export function ShopDetailPage() {
-  const { shopId } = useParams();
+  // const { shopId } = useParams();
+  const { state: shop } = useLocation() as Location<ShopData | undefined>;
 
-  if (shopId !== "1") {
-    return <PageBase>Not Found</PageBase>;
+  if (!shop) {
+    return <div>TODO: 飲食店詳細情報取得</div>;
   }
-
-  const sampleShop = {
-    id: 1,
-    name: "仙人",
-    image_url: "https://via.placeholder.com/800x500",
-    area: { id: 1, name: "東京都" },
-    genre: { id: 1, name: "寿司" },
-    detail:
-      "料理長厳選の食材から作る寿司を用いたコースをぜひお楽しみください。食材・味・価格、お客様の満足度を徹底的に追及したお店です。特別な日のお食事、ビジネス接待まで気軽に使用することができます。",
-    favorite_status: "unknown"
-  } as ShopData;
 
   return (
     <PageBase wrapperStyle={wrapperStyle}>
-      <ShopDetailArea shop={sampleShop} />
+      <ShopDetailArea shop={shop} />
       <ShopReservationArea />
     </PageBase>
   );
