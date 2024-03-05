cd87143a269a399912a7ea3f7dc7da4caf1cd97c
ryamao <yamao.ryouta@gmail.com>
2024-03-05 11:44:37 +0900

    飲食店詳細ページの予約情報カードの外観を追加する

---
+++ src/components/ReservationCard.tsx
@@ -0,0 +1,50 @@
+import styled from "@emotion/styled";
+import dayjs from "dayjs";
+
+import { ReservationData } from "../models";
+
+export interface ReservationCardProps {
+  reservation: ReservationData;
+}
+
+export function ReservationCard({
+  reservation: { shop, reserved_at, number_of_guests }
+}: ReservationCardProps) {
+  const datetime = dayjs(reserved_at);
+
+  return (
+    <Table>
+      <tr>
+        <Head>Shop</Head>
+        <td>{shop.name}</td>
+      </tr>
+      <tr>
+        <Head>Date</Head>
+        <td>{datetime.format("YYYY-MM-DD")}</td>
+      </tr>
+      <tr>
+        <Head>Time</Head>
+        <td>{datetime.format("HH:mm")}</td>
+      </tr>
+      <tr>
+        <Head>Number</Head>
+        <td>{number_of_guests}人</td>
+      </tr>
+    </Table>
+  );
+}
+
+const Table = styled.table`
+  width: 100%;
+  padding: 1.25rem;
+  color: white;
+  background-color: #4d7fff;
+  border-radius: 0.25rem;
+`;
+
+const Head = styled.th`
+  width: 20%;
+  padding: 0.25rem 0;
+  font-weight: normal;
+  text-align: left;
+`;
