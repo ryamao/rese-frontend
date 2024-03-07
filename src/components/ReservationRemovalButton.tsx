2834e94a7a71124d181db06b45bb8075b824bad8
ryamao <yamao.ryouta@gmail.com>
2024-03-07 16:45:18 +0900

    予約状況カードの削除ボタンの外観を追加する

---
+++ src/components/ReservationRemovalButton.tsx
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+
+export function ReservationRemovalButton() {
+  return (
+    <Button>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        width="24"
+        height="24"
+        stroke="#fff"
+      >
+        <circle cx="50" cy="50" r="45" fill="none" strokeWidth="9" />
+        <line x1="33" y1="33" x2="67" y2="67" strokeWidth="9" />
+        <line x1="33" y1="67" x2="67" y2="33" strokeWidth="9" />
+      </svg>
+    </Button>
+  );
+}
+
+const Button = styled.button`
+  cursor: pointer;
+  background: none;
+  border: none;
+`;
