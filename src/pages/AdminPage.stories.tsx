cf87eb459e115504dc48842345b16b77226ddc00
ryamao <yamao.ryouta@gmail.com>
2024-03-16 20:21:22 +0900

    管理者ページの店舗代表者アカウント作成エリアのUIを追加する

---
+++ src/pages/AdminPage.stories.tsx
@@ -0,0 +1,55 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MemoryRouter, Outlet, Route, Routes } from "react-router-dom";
+
+import { AdminPage } from "./AdminPage";
+import {
+  BackendAccessContext,
+  createBackendAccessContextType
+} from "../contexts/BackendAccessContext";
+import { HttpClient } from "../HttpClient";
+import { handlers } from "../mocks/handlers";
+
+const meta = {
+  title: "Pages/AdminPage",
+  component: AdminPage,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+    msw: {
+      handlers
+    }
+  },
+  decorators: [
+    (Story) => {
+      const queryClient = new QueryClient();
+      const backendAccess = createBackendAccessContextType({
+        httpClient: new HttpClient(),
+        authStatus: { status: "customer", id: 1, has_verified_email: true },
+        setAuthStatus: fn()
+      });
+      const DummyRoute = () => (
+        <Outlet context={{ authStatus: { status: "customer", id: 1 } }} />
+      );
+      return (
+        <QueryClientProvider client={queryClient}>
+          <BackendAccessContext.Provider value={backendAccess}>
+            <MemoryRouter>
+              <Routes>
+                <Route element={<DummyRoute />}>
+                  <Route path="/" element={<Story />} />
+                </Route>
+              </Routes>
+            </MemoryRouter>
+          </BackendAccessContext.Provider>
+        </QueryClientProvider>
+      );
+    }
+  ]
+} satisfies Meta<typeof AdminPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
